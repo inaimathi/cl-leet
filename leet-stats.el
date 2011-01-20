8bf0a65d123f4e6c8e514c9d10ceee5e6a4c1cca
Leo Zovic <ingram@nomad.gateway.2wire.net>
2011-01-20 17:22:09 -0500

    -Added name-related functions to leet-stats (also added list->data function to convert flat lists to graphable name/value pairs)

--- leet-stats.el
+++ leet-stats.el
@@ -8,6 +8,13 @@
 (defvar *test-gal* nil)
 (defvar *stats* nil)
 
+(defun generate-name-data ()
+  (interactive)
+  (with-current-buffer (get-buffer-create "*graph*")
+    (let ((names (mapcar 'grammar->string (make-list 3000 planet-name-grammar))))
+      (print-graph (cons "planet-names" (list->data names))))
+    (display-buffer "*graph*")))
+
 (defun generate-data-set ()
   (interactive)
   (progn (reset-data)
@@ -21,13 +28,41 @@
 		    (tech-level . nil)
 		    (population . nil)
 		    (market-mod . nil)
-		    (name . nil)
-		    (description . nil)
 		    (radius . nil)))))
 
 (defun graph-galaxy (gal)
   (progn (planets->stats! gal)
 	 (display-stats *stats*)))
+
+(defun planets->stats! (list-of-planets)
+  (mapc 'add-planet! list-of-planets))
+
+(defun add-planet! (p)
+  (progn
+    (add-stat! 'government (planet-government p))
+    (add-stat! 'economy (planet-economy p))
+    (add-stat! 'tech-level (planet-tech-level p))
+    (add-stat! 'population (planet-population p))
+    (add-stat! 'market-mod (max 10 (+ (- (* (planet-economy p) (planet-productivity p)) (planet-government p)) (planet-tech-level p))))
+    (add-stat! 'radius (planet-radius p))))
+
+(defun add-stat! (stat addition)
+  (let ((existing (cdr (assoc addition (cdr (assoc stat *stats*))))))
+    (if existing
+	(incf (cdr (assoc addition (cdr (assoc stat *stats*)))))
+      (setf (cdr (assoc stat *stats*)) (cons `(,addition . 1) (cdr (assoc stat *stats*)))))))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Graph Printing (game-independant) ;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(defun list->data (a-list)
+  (let ((acc nil))
+    (progn (mapc (lambda (elem)
+		   (let ((existing (assoc elem acc)))
+		     (cond (existing (incf (cdr existing)))
+			   (t (setf acc (cons `(,elem . 1) acc))))))
+		 a-list)
+	   acc)))
 
 (defun display-stats (stat-alist)
   (progn (with-current-buffer (get-buffer-create "*graph*")
@@ -41,27 +76,7 @@
 					  (if (stringp (car a))
 					      (string< (car a) (car b))
 					    (< (car a) (car b))))))))
-    (progn (insert (format "\n\n%10s\n" title))
+    (progn (insert (format "\n\n%20s\n" title))
 	   (mapc (lambda (a) (insert (format "%10s: %s\n" (car a) (cdr a)))) data))))
-
-(defun planets->stats! (list-of-planets)
-  (mapc 'add-planet! list-of-planets))
-
-(defun add-planet! (p)
-  (progn
-    (add-stat! 'government (planet-government p))
-    (add-stat! 'economy (planet-economy p))
-    (add-stat! 'tech-level (planet-tech-level p))
-    (add-stat! 'population (planet-population p))
-    (add-stat! 'market-mod (max 10 (+ (- (* (planet-economy p) (planet-productivity p)) (planet-government p)) (planet-tech-level p))))
-    (add-stat! 'name (planet-name p))
-    (add-stat! 'description (planet-description p))
-    (add-stat! 'radius (planet-radius p))))
-
-(defun add-stat! (stat addition)
-  (let ((existing (cdr (assoc addition (cdr (assoc stat *stats*))))))
-    (if existing
-	(incf (cdr (assoc addition (cdr (assoc stat *stats*)))))
-      (setf (cdr (assoc stat *stats*)) (cons `(,addition . 1) (cdr (assoc stat *stats*)))))))
 
 (provide 'leet-stats)
